b8cf6db46579fe0220e3a51579dbafe5d606ee23
TebogoMothelo <tebogo.mothelo@umuzi.org>
2020-01-08 10:58:37 +0200

    removed comments and unused code

--- techon/src/Components/Regions/Navigation/Navigation.js
+++ techon/src/Components/Regions/Navigation/Navigation.js
@@ -6,7 +6,6 @@ import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -17,7 +16,6 @@ import FaceIcon from '@material-ui/icons/Face';
 import HomeIcon from '@material-ui/icons/Home';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 const drawerWidth = 240;
@@ -139,10 +137,7 @@ function NavigationBar(props) {
 }
 
 NavigationBar.propTypes = {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
+
   container: PropTypes.instanceOf(typeof Element === 'undefined' ? Object : Element),
 };
 
