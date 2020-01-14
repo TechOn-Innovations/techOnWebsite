746b67bd3a249cf2f37f010cde6d33df6b0d2637
PearlSithebe <pearlsithebe@gmail.com>
2020-01-14 13:20:54 +0200

    added a footer to the page layout

--- techon/src/App.js
+++ techon/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavigationBar from './Components/Regions/Navigation/Navigation';
+import Footer from './Components/Regions/Footer/Footer';
 
 function App() {
   return (
     <React.Fragment>
       < NavigationBar />
+      <Footer/>
     </React.Fragment>
   );
 }
