a4e2693be6f78075b1d24622a3abd560573496d3
TebogoMothelo <tebogo.mothelo@umuzi.org>
2020-01-16 14:14:30 +0200

    added placeholder image and centered grid

--- techon/src/Components/Regions/Services/Services.js
+++ techon/src/Components/Regions/Services/Services.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -9,25 +8,21 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import TechOn from './TechOn.jpg';
+
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
     card: {
-      align: 'right'
-  }
+      align: 'center',
+  },
 }));
 
 function Services() {
   const classes = useStyles();
-
   return (
-    <div className={classes.root}>
+    <div style={{marginLeft: '400px'}} className={classes.root}>
       <h1>Our Services</h1>
       <Grid container spacing={3}>
         <Grid item xs={5}>
@@ -35,14 +30,14 @@ function Services() {
       <CardActionArea>
         <CardMedia
           component="img"
-          alt="Contemplative Reptile"
+          alt="PlaceHolder"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
+          image={TechOn}
+          title="PlaceHolder"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Lizard
+            Web Development
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
             Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
@@ -52,10 +47,7 @@ function Services() {
       </CardActionArea>
       <CardActions>
         <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
+          More
         </Button>
       </CardActions>
     </Card>
@@ -65,14 +57,14 @@ function Services() {
       <CardActionArea>
         <CardMedia
           component="img"
-          alt="Contemplative Reptile"
+          alt="PlaceHolder"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
+          image={TechOn}
+          title="PlaceHolder"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Lizard
+            App Development
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
             Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
@@ -82,10 +74,7 @@ function Services() {
       </CardActionArea>
       <CardActions>
         <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
+          More
         </Button>
       </CardActions>
     </Card>
@@ -95,14 +84,14 @@ function Services() {
       <CardActionArea>
         <CardMedia
           component="img"
-          alt="Contemplative Reptile"
+          alt="PlaceHolder"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
+          image={TechOn}
+          title="PlaceHolder"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Lizard
+           Prototypes
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
             Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
@@ -112,10 +101,7 @@ function Services() {
       </CardActionArea>
       <CardActions>
         <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
+          More
         </Button>
       </CardActions>
     </Card>
@@ -125,14 +111,14 @@ function Services() {
       <CardActionArea>
         <CardMedia
           component="img"
-          alt="Contemplative Reptile"
+          alt="PlaceHolder"
           height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
+          image={TechOn}
+          title="PlaceHolder"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Lizard
+            Logo 	&amp; Poster Design
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
             Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
@@ -142,10 +128,7 @@ function Services() {
       </CardActionArea>
       <CardActions>
         <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
+          More
         </Button>
       </CardActions>
     </Card>
