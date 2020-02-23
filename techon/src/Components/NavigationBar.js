29ef451c77dce70a37342e3d3ad1f0a2826628a5
TebogoMothelo <tebogo.mothelo@umuzi.org>
2020-02-23 13:29:19 +0200

    added temp top nav for routing

--- techon/src/Components/NavigationBar.js
+++ techon/src/Components/NavigationBar.js
@@ -1,34 +1,31 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import '../App.css'
-import logo from '../logo.svg'
+import "../App.css";
+import logo from "../logo.png";
 
-
-function NavBar () {
-    return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="#home">
-           <img
-            alt=""
-            src={logo}
-            width="50"
-            height="50"
-            className="d-inline-block align-top"
-          /></Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="mr-auto">
-    <Nav.Link href="/">TechOnInnovations</Nav.Link>
-    </Nav>
-    <Nav>
-      <Nav.Link href="/about">About</Nav.Link>
-      <Nav.Link href="/services">Services</Nav.Link>
-      <Nav.Link href="/contact">Contact</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-    );
+function NavBar() {
+  return (
+    <Navbar bg="dark" variant="dark" className="custom-nav">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav>
+          <Nav.Link href="/" className="custom-link">
+            TechOnInnovations
+          </Nav.Link>
+          <Nav.Link className="custom-link" href="/about">
+            About
+          </Nav.Link>
+          <Nav.Link className="custom-link" href="/services">
+            Services
+          </Nav.Link>
+          <Nav.Link className="custom-link" href="/contact">
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default NavBar;
